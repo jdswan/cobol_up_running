2cfd5197e29367e3628ff5614c8b2e2d179d51a6
JDSwan <jeffdswan@gmail.com>
2019-06-25 20:36:34 -0500

    Using padded display values so things look a lot better. Increased size of WS-AREA so the calc no longer overflows on any two-digit values.

--- 03_04_challenge/challenge.cbl
+++ 03_04_challenge/challenge.cbl
@@ -18,17 +18,23 @@
         01 WS-CONSTANTS.
              05 WS-PI PIC 9V99999 VALUE 3.14159.
         01 WS-RESULTS.
-             05 WS-CIRCUM PIC 9999V9999.
-             05 WS-AREA PIC 9999V99999.
+             05 WS-CIRCUM PIC 9999V99999.
+             05 WS-AREA PIC 999999V99999.
+        01 WS-DISPLAY.
+             05 WS-DISPLAY-CIRCUM PIC ZZ99.99999.
+             05 WS-DISPLAY-AREA PIC ZZZZ99.99999.
 
        PROCEDURE DIVISION.
        0100-CALCULATE-PI.
 
            DISPLAY "Enter the radius of the circle:".
            ACCEPT WS-RADIUS.
-           COMPUTE WS-CIRCUM = 2 * WS-PI * WS-RAIUS.
+           COMPUTE WS-CIRCUM = 2 * WS-PI * WS-RADIUS.
+           MOVE WS-CIRCUM TO WS-DISPLAY-CIRCUM.
            COMPUTE WS-AREA = WS-PI * WS-RADIUS * WS-RADIUS.
-           DISPLAY "The circumpherence of the circle is ", WS-CIRCUM.
-           DISPLAY "The area of the circle is ", WS-AREA.
+           MOVE WS-AREA TO WS-DISPLAY-AREA.
+           DISPLAY "The circumpherence of the circle is ",
+                   WS-DISPLAY-CIRCUM.
+           DISPLAY "The area of the circle is ", WS-DISPLAY-AREA.
 
            STOP RUN.
